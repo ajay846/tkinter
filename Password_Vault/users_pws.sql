50da087b52f565b7a0c33c054944ca70056c2b39
SAI <66007868+ajay846@users.noreply.github.com>
2020-10-24 12:53:23 +0530

    users_pws.sql

--- Password_Vault/users_pws.sql
+++ Password_Vault/users_pws.sql
@@ -2,13 +2,13 @@ use main;
 
 create table users_pws(
 	id INT primary key auto_increment,
-    for_website varchar(1000),
-    username varchar(1000),
-    password_ varchar(1000)
+    	for_website varchar(1000),
+    	username varchar(1000),
+    	password_ varchar(1000)
 );
 
 create table mainPass(
-    password_ varchar(2000)
+    	password_ varchar(2000)
 );
 
 ALTER TABLE mainPass ADD COLUMN id INT PRIMARY KEY;
